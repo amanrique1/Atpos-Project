2799ce51c2eedbe09235396cf9269926c2971ede
Geovanny Andrés González <ga.gonzalezr@uniandes.edu.co>
2019-03-17 00:17:01 -0500

    Listo las pruebas basicas del requerimiento de disponibilidad

--- Producto/views.py
+++ Producto/views.py
@@ -22,9 +22,14 @@ def index(request):
     return render(request, 'base.html')
 
 def darProductos(request):
-    verificarConexion('local', Producto) #Realizar verificacion de la DB local para migracion
+    verificarMigracion('default', 'local', Producto) #Realizar verificacion de la DB local para migracion
 
-    queryset = Producto.objects.all().order_by('-marca')[:10] #QuerySets para interactuar con el SMBD
+    queryset = None #Inicializar variable
+
+    if verificarConexion('default'):
+        queryset = Producto.objects.all().order_by('-marca')[:10] #QuerySets para interactuar con el SMBD
+    else:
+        queryset = Producto.objects.using('local').all().order_by('-marca')[:10] #QuerySets para interactuar con el SMBD
     
     #Ejemplo de un QuerySet para cambiar de BD 
     
@@ -38,7 +43,7 @@ def darProductos(request):
     return render(request, 'Producto/darProducto.html', context) #Ojo con el HTML
 
 def crearProducto(request):
-    verificarConexion('local', Producto) #Realizar verificacion de la DB local para migracion
+    verificarMigracion('default', 'local', Producto) #Realizar verificacion de la DB local para migracion
 
     if request.method == 'POST':
         form = ProductoForm(request.POST)
@@ -52,7 +57,7 @@ def crearProducto(request):
                 producto.save()
             except (OperationalError, DatabaseError) as excepcion: #Error producido por la mala operacion de la BD principal
                 print("Se ha perdido la conexion a la base de datos global, cambiando a sistema auxiliar")
-                print(str(excepcion)) #Imprimir el error
+                # print(str(excepcion)) Imprimir el error
                 producto.save(using='local') #Guardar la informacion en la base de datos local                        
             
             messages.add_message(request, messages.SUCCESS, 'El producto ha sido creado satisfactoriamente')
@@ -68,40 +73,39 @@ def crearProducto(request):
 
     return render(request, 'Producto/crearProducto.html', context)
 
-def migrarDBlocal(model):   
+def migrarDBlocal(model):
 
-    #Verificar si hay datos para migrar
-    if model.objects.count() == 0:
-        print("[Migrar DB Local] La DB auxiliar para el modelo pasado por parametro esta vacia, no es necesario migrar")
+    global migrando #Hacer referencia a la variable global "migrando"  
     
-    else:
-        #Construir un QuerySet para recorrer todos los valores presentes en la DB local
-        querySet = model.objects.using('local').all() #Traer todos los valores a migrar
+    #Construir un QuerySet para recorrer todos los valores presentes en la DB local
+    querySet = model.objects.using('local').all() #Traer todos los valores a migrar
     
-        #Variable para listar
-        i = 0   
+    #Variable para listar
+    i = 0   
 
-        for registro in querySet:
-            try:
-                i += 1
-                dict_data = ProductoSerializer(registro).data
-                print("\n ===== Migracion a la base de datos global ======= \n")
-                print(str(i) + "). " + imprimirValores(dict_data))        
-                print("Almacenando registro en la base de datos global")
-                registro.save()
-                #Borrar el registro de la base de datos local
-                registro.delete(using='local') #Ojo que el parametro using indica la DB donde se ejecuta la operacion        
-                print("Registro borrado de la base de datos local")
-                print("=============================================== \n")
+    for registro in querySet:
+        try:
+            i += 1
+            dict_data = ProductoSerializer(registro).data
+            print("\n ===== Migracion a la base de datos global ======= \n")
+            print(str(i) + "). " + imprimirValores(dict_data))        
+            print("Almacenando registro en la base de datos global")
+            registro.save(using='default')
+            #Borrar el registro de la base de datos local
+            registro.delete(using='local') #Ojo que el parametro using indica la DB donde se ejecuta la operacion        
+            print("Registro borrado de la base de datos local")
+            print("=============================================== \n")
             
-            except OperationalError:            
-                print("[Migrar DB Local] La conexion a la base de datos ha fallado, esperando reconexion")
-                evento_wait.wait()    
+        except (OperationalError, DatabaseError) as excepcion:            
+            print("[Migrar DB Local] La conexion a la base de datos ha fallado, esperando reconexion")
+            evento_wait.wait()    
 
     #Terminacion exitosa
     print("[Migrar DB Local] La migracion para los datos del modelo ha sido exitosa")
     mutex.acquire()
     migrando = False
+    if migrando is not False:
+        print("[Migrar DB Local] Error faltal de candados")
     mutex.release()
 
 #Devuelve una cadena de texto con los datos del dicccionario
@@ -113,7 +117,37 @@ def imprimirValores(diccionario):
     return cadena_respuesta
 
 #Verifica si hay conexion al servidor central, en caso de que la haya migrar la base de datos auxiliar
-def verificarConexion(idDB, model):
+def verificarMigracion(idDB, idDBLocal, model):
+
+    if model.objects.using(idDBLocal).count() == 0:
+        print("[Verificar-Migracion] La base de datos auxiliar no tiene registros sobre el modelo") 
+        return None   
+    
+    global migrando #Hacer referencia a la variable global "migrando"
+    hay_conexion = verificarConexion(idDB)
+    
+    if hay_conexion:        
+        #Exclusion mutua - consultar si hay threads migrando
+        evento_wait.set() #Despertar al thread de migracion si se durmio porque hubo un error (Hacer notify())
+        mutex.acquire()       
+        if migrando == True:
+            #Liberar recurso y terminar - ya hay alguien haciendo el proceso
+            print("[Verificar-Migracion] Ya se encuentra en progreso el proceso de migracion")
+            mutex.release()
+            return None
+        else: #No hay nadie migrando -  cambiar estado y comenzar a migrar
+            migrando = True
+            mutex.release() #Liberar candado
+            print("[Verificar-Migracion] Lanzando proceso de migracion")
+            nuevoHilo = Thread(name="Geovanny", target=migrarDBlocal, args=(model,)) #Objeto
+            nuevoHilo.start()
+    
+# Permite verificar la conexion a una DB existente
+# @param idDB - Es el identificador de la base de datos que se desea verificar conexion
+# return True si hay conexion a la base de datos deseada, False de lo contrario
+
+def verificarConexion(idDB):
+    
     #Declarar cursor para conexion a la DB
     enlace = connections[idDB] #Default es la DB Global
     hay_conexion = False
@@ -121,30 +155,15 @@ def verificarConexion(idDB, model):
     #Conectar y obtener cursor
     try:
        cursor = enlace.cursor()
-    except OperationalError as error:
-        print("[Verificar-Conexion] La conexion a la base de datos %s ha fallado", idDB)
+    except (OperationalError, DatabaseError) as excepcion:
+        print("[Verificar-Conexion] La conexion a la base de datos " + idDB + " ha fallado",)
     else:
         #Conexion exitosa
-        hay_conexion = True
-        evento_wait.set() #Despertar al thread de migracion si se durmio porque hubo un error
+        hay_conexion = True        
         cursor.close()
 
-    if hay_conexion:        
-        #Exclusion mutua - consultar si hay threads migrando
-        mutex.acquire()        
-        if migrando == True:
-            #Liberar recurso y terminar - ya hay alguien haciendo el proceso
-            print("[Verificar-Conexion] Ya se encuentra en progreso el proceso de migracion")
-            mutex.release()
-            return None
-        else: #No hay nadie migrando -  cambiar estado y comenzar a migrar
-            migrando = True
-            mutex.release() #Liberar candado
-            print("[Verificar-Conexion] Lanzando proceso de migracion")
-            nuevoHilo = Thread(name="Geovanny", target=migrarDBlocal, args=(model,)) #Objeto
-            nuevoHilo.start()
+    return hay_conexion
     
-
         
 
 

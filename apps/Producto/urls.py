20669a6e16101c134508d6fcf5838a20036eaf92
Andrés Manrique <andress0199@outlook.com>
2019-04-15 15:49:02 -0500

    se quito los except del token de seguridad

--- apps/Producto/urls.py
+++ apps/Producto/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from django.conf.urls import url, include
-from django.views.decorators.csrf import csrf_exempt
 
 from apps.Producto.views import *
 
 urlpatterns = [
     url('^$', index),
     url(r'^darProductos/', darProductos), #Vista asociada
-    url(r'^crearProducto/$', csrf_exempt(crearProducto), name='crearPorducto'),  
+    url(r'^crearProducto/$', crearProducto, name='crearPorducto'),  
     url(r'^darEspecificacionesProducto/', darEspecificacacionesProductos), #Vista asociada
-    url(r'^crearEspecificacionesProducto/$', csrf_exempt(crearEspecificacionProducto), name='crearEspecificacionProducto'),
+    url(r'^crearEspecificacionesProducto/$', crearEspecificacionProducto, name='crearEspecificacionProducto'),
     url(r'^darInventario/', darEspecificacacionesProductos), #Vista asociada
-    url(r'^crearInventario/$', csrf_exempt(crearInventario), name='crearEspecificacionProducto'),        
+    url(r'^crearInventario/$',crearInventario, name='crearEspecificacionProducto'),        
 ]
